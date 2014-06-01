94a9e834d943b6a58d78ec3a6510ee1f077aa433
Jamie Collinson <jamiecollinson@gmail.com>
2014-06-01 16:38:30 +0000

    csv working

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 var csv = require('ya-csv');
 
 var reader = csv.createCsvFileReader('results.csv', { columnsFromHeader: true });
+
+var teams = {};
 reader.addListener('data', function(data) {
-  console.log(data);
+  teams[data.home_team] = teams[data.home_team] || {'pi_home': 1, 'pi_away': 1};
+});
+reader.addListener('end', function(data) {
+  console.log(teams);
 });
